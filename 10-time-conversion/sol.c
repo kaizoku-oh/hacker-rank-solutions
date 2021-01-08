15d40dced60953eed96f9e5d75ee9a33301cc415
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-01-08 18:36:04 +0100

    adding challenge

---
+++ 10-time-conversion/sol.c
@@ -0,0 +1,57 @@
+char* timeConversion(char *s)
+{
+  int hh;
+  int mm;
+  int ss;
+  char am_pm;
+  static char out[sizeof("0hh:0mm:0ss")];
+  char tcHh[3];
+  char tcMm[3];
+  char tcSs[3];
+
+  if(s)
+  {
+    sscanf(s, "%d:%d:%d%cM", &hh, &mm, &ss, &am_pm);
+    if('P' == am_pm)
+    {
+      if(hh > 12)
+      {
+        hh = (hh + 12) - 24;
+      }
+      else if(hh < 12)
+      {
+        hh += 12;
+      }
+    }
+    else if(('A' == am_pm) && (12 == hh))
+    {
+      hh = 0;
+    }
+    if(hh < 10)
+    {
+      snprintf(tcHh, sizeof("hh"), "0%d", hh);
+    }
+    else
+    {
+      snprintf(tcHh, sizeof("hh"), "%d", hh);
+    }
+    if(mm < 10)
+    {
+      snprintf(tcMm, sizeof("mm"), "0%d", mm);
+    }
+    else
+    {
+      snprintf(tcMm, sizeof("mm"), "%d", mm);
+    }
+    if(ss < 10)
+    {
+      snprintf(tcSs, sizeof("ss"), "0%d", ss);
+    }
+    else
+    {
+      snprintf(tcSs, sizeof("ss"), "%d", ss);
+    }
+    snprintf(out, sizeof("hh:mm:ss"), "%s:%s:%s", tcHh, tcMm, tcSs);
+  }
+  return out;
+}
